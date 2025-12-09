de2ef90a01d8dd6db797632f9e50c11c3ce3adaf
bruhkage <joshuaoconnor115@gmail.com>
2025-12-09 15:09:05 +0000

    added function to introduce myself

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def introduce_student():
     <p>Hello! I'm a student using Flask.</p>
     <p>This is a simple web application built with Flask.</p>
             '''
+@app.route('/Josh')
+def introduce_josh():
+    return '''
+    <h1>Student Introduction</h1>
+    <p>Hello! I'm josh using Flask.</p>
+    <p>This is a simple web application built with Flask mmmmmmmmm.</p>
+            '''
 
 if __name__ == '__main__':
     app.run(debug=True)

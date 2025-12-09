9d3007818700b5c44dcc3c789370d4d514c2f60b
ShuntGit <simon.hunt@solihull.ac.uk>
2025-12-09 13:17:53 +0000

    initial flask commit
    Add a simple Flask web application that introduces a student.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def introduce_student():
+    return '''
+    <h1>Student Introduction</h1>
+    <p>Hello! I'm a student using Flask.</p>
+    <p>This is a simple web application built with Flask.</p>
+            '''
+
+if __name__ == '__main__':
+    app.run(debug=True)

780f104fc3051a352e562ffbd8aca4680802aa90
AlexJenkins0 <jen24001768@students.solihull.ac.uk>
2025-12-09 15:12:05 +0000

    I added my own route to the original work.

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ def introduce_student():
     <p>This is a simple web application built with Flask.</p>
             '''
 
+@app.route('/Alex')
+def introduce_student():
+    return '''
+    <h1>Hello my name is Alex</h1>
+    <p>I am a student who is learning Flask and GitHub.</p>
+    <p>This is a simple web application that was built up with Flask.</p>
+            '''
+
+
 if __name__ == '__main__':
     app.run(debug=True)
